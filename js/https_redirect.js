a27cafb336325c738cccd55c6661e9d40a606ba4
PiGroup 3.14 <72083798+pigroupco@users.noreply.github.com>
2024-07-24 21:16:54 +0700

    Update https_redirect.js

--- js/https_redirect.js
+++ js/https_redirect.js
@@ -1,7 +1,9 @@
 var links = document.links;
-
 for (var i = 0, linksLength = links.length; i < linksLength; i++) {
-   if (links[i].hostname != window.location.hostname) {
-       links[i].href = links[i].href.replace('http://', 'https://')
-   } 
+   links[i].href = links[i].href.replace('http://', 'https://');
+}
+
+var forms = document.forms;
+for (var i = 0, formsLength = forms.length; i < formsLength; i++) {
+   forms[i].action = forms[i].action.replace('http://', 'https://');
 }
